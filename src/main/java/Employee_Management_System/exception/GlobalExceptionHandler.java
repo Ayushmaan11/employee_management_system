d3ce80aa3cc9b3ba5e71afd605b24b52441d8759
Ayushmaan Pandey <maanayush478@gmail.com>
2026-08-18 22:43:13 +0530

    Added proper valiation and error pipeline

--- src/main/java/Employee_Management_System/exception/GlobalExceptionHandler.java
+++ src/main/java/Employee_Management_System/exception/GlobalExceptionHandler.java
@@ -1,15 +1,38 @@
 package Employee_Management_System.exception;
 
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleValidationException(MethodArgumentNotValidException  ex){
+        Map<String,String> errors = ex.getBindingResult().getFieldErrors().stream()
+                .collect(java.util.stream.Collectors.toMap(
+                        error -> error.getField(),
+                        error -> error.getDefaultMessage()
+                ));
+
+        return new ErrorResponse(
+                400,
+                "Validation Failed",
+                errors
+        );
+    }
+
     @ExceptionHandler(ResourceNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String handleResourceNotFound(ResourceNotFoundException ex){
-        return ex.getMessage();
+    public ErrorResponse handleResourceNotFound(ResourceNotFoundException ex){
+        return new ErrorResponse(
+                404,
+                ex.getMessage()
+        );
     }
 }
